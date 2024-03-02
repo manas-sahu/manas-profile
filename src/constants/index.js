6ba6db237f49e2b644e014b41d42926002ba6b90
Manas Sahu <manas.sahu.one@gmail.com>
2024-03-02 12:56:09 +0100

    Updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,19 +46,19 @@ export const navLinks = [
 
 const services = [
   {
-    title: "Web Developer",
+    title: "Frontend Developer",
     icon: web,
-  },
-  {
-    title: "React Native Developer",
-    icon: mobile,
   },
   {
     title: "Backend Developer",
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "React Developer",
+    icon: mobile,
+  },
+  {
+    title: "Lead Engineer",
     icon: creator,
   },
 ];
